179c77cd86d04a55b480c16662d245ecef538f91
Vlad Vovk <wolfykey@gmail.com>
2020-03-29 16:39:44 +0300

    add middleware

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import thunk from 'redux-thunk'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { rootReducer } from './redux/rootReducer'
+import { forbiddenWordsMiddleware } from './redux/middleware'
 
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
+    applyMiddleware(thunk, forbiddenWordsMiddleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 )
---
+++ src/redux/middleware.js
@@ -0,0 +1,20 @@
+import { CREATE_POST } from './types'
+import { showAlert } from './actions'
+
+const forbidden = ['fuck', 'php']
+
+export function forbiddenWordsMiddleware({ dispatch }) {
+  return function(next) {
+    return function(action) {
+      if (action.type === CREATE_POST) {
+        const found = forbidden.filter(word =>
+          action.payload.title.includes(word)
+        )
+        if (found.length) {
+          return dispatch(showAlert('Spam detected!'))
+        }
+      }
+      return next(action)
+    }
+  }
+}
